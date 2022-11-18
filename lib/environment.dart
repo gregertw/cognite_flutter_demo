4e079e40c108b036b8bcbde1f5ed4e416f3820d3
Greger Wedel <greger@greger.io>
2022-11-18 08:37:19 +0100

    Update clientid to new app registration in new AAD

--- lib/environment.dart
+++ lib/environment.dart
@@ -5,7 +5,7 @@ class Environment {
       defaultValue:
           "BCKRpKeIZ7-0iWQGbOe6rcTGW1kvAGbyDNPH4Waw-Yzs2cRPJ5xsKUvoybBp2l7KIBx7W2SYh1T9kKh3dlG2KHw");
   static const String clientIdAAD = String.fromEnvironment("CLIENTID_AAD",
-      defaultValue: "05dc6b4f-5197-4ab9-9bd3-a515035237bf");
+      defaultValue: "1d14eab0-4754-4be1-b9c6-63fdadddf778");
   static const String secretAAD =
       String.fromEnvironment("SECRET_AAD", defaultValue: "");
 }
